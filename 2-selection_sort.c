64129cfaf12ae7ab5a7a3ef8ad378ecf414e08d2
Kandrianaha <k.andria@icloud.com>
2025-08-12 08:23:46 +0000

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,24 +8,26 @@
  */
 void selection_sort(int *array, size_t size)
 {
+	size_t i, j, min_idx;
+	int temp;
+
 	if (array == NULL || size < 2)
 		return;
 
-	for (size_t i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		size_t min_index = i;
-
-		for (size_t j = i + 1; j < size; j++)
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[min_index])
-				min_index = j;
+			if (array[j] < array[min_idx])
+				min_idx = j;
 		}
 
-		if (min_index != i)
+		if (min_idx != i)
 		{
-			int temp = array[i];
-			array[i] = array[min_index];
-			array[min_index] = temp;
+			temp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
 
 			print_array(array, size);
 		}
